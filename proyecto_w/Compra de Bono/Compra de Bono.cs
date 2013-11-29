d52cd523849312cad650d091e7e1f3c1292727d8
aritama <aritama@ymail.com>
2013-11-28 22:54:12 -0300

    compra de bono por parte del admin

--- proyecto_w/Compra de Bono/Compra de Bono.cs
+++ proyecto_w/Compra de Bono/Compra de Bono.cs
@@ -29,20 +29,29 @@ namespace proyecto_w.Compra_de_Bono
         private void btnCDB_RealizarCompra_Click(object sender, EventArgs e)
         {
             // CHEQUEAR AFIL_NRO, CANTIDAD > 0 , ANTES DE ENVIAR LA QUERY
+            lblCDB_Status.Text = "HACIENDO";
             ConexionSQL connectionSQL = ConexionSQL.Instance;
-           
+                                   
             String queryCompra = string.Format("exec PROYECTO_W.SP_COMPRABONOADMIN {0},'{1}',{2},'2014-9-9'",
-                txtCDB_AfilNro.Text, cmbCDB_Tipo.SelectedText, txtCDB_Cantidad.Text);
+                txtCDB_AfilNro.Text, cmbCDB_Tipo.Text, txtCDB_Cantidad.Text);
+            String querySuma = string.Format("SELECT PROYECTO_W.F_COMPRABONO_SUMA_ULTIMA ({0},'{1}',{2},'2014-9-9')",
+                txtCDB_AfilNro.Text, cmbCDB_Tipo.Text, txtCDB_Cantidad.Text);
             // TENDRIA QUE ENVIARSE LA FECHA ACTUAL, PERO ESTAMO PROBANDO NOMA
             
+            
             try
-            { connectionSQL.ejecutarQuery(queryCompra); }
+            { 
+                connectionSQL.ejecutarQuery(queryCompra);
+            }
             catch (SqlException )
              {
                 lblCDB_Status.Text = "Datos no validos";   
              }
-            
-            // OBTENER LO QUE PAGARÍA POR LA COMPRA EL AFILIADO, pa mostra por pantalla e noma
+            if (lblCDB_Status.Text != "Datos no validos")
+            {
+                DataTable suma = connectionSQL.ejecutarQuery(querySuma);
+                lblCDB_Status.Text = "Compra realizada, suma a pagar: " + suma.Rows[0][0].ToString();
+            }
             
         }
 
