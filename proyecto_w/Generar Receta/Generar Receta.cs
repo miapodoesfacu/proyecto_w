d0651b144a23e4bd6906a473b6d39fdca33971c3
aritama <arislvrchr@gmail.com>
2013-12-07 02:52:20 -0300

    suo y sln sacamo

--- proyecto_w/Generar Receta/Generar Receta.cs
+++ proyecto_w/Generar Receta/Generar Receta.cs
@@ -20,7 +20,6 @@ namespace proyecto_w.Generar_Receta
 
         private void Generar_Receta_Load(object sender, EventArgs e)
         {
-            textBox1.Text = turno_nro.ToString();
         }
     }
 }
