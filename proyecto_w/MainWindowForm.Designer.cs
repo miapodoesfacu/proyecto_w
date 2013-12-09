5ccc277adc336fad0cb6a3f5c060503ff7bd87ba
miapodoesfacu <welschen.facundo@gmail.com>
2013-12-08 21:10:00 -0300

    Modificando inicio

--- proyecto_w/MainWindowForm.Designer.cs
+++ proyecto_w/MainWindowForm.Designer.cs
@@ -42,9 +42,9 @@
             // 
             // btnABMAfiliado
             // 
-            this.btnABMAfiliado.Location = new System.Drawing.Point(12, 12);
+            this.btnABMAfiliado.Location = new System.Drawing.Point(12, 58);
             this.btnABMAfiliado.Name = "btnABMAfiliado";
-            this.btnABMAfiliado.Size = new System.Drawing.Size(75, 23);
+            this.btnABMAfiliado.Size = new System.Drawing.Size(183, 40);
             this.btnABMAfiliado.TabIndex = 0;
             this.btnABMAfiliado.Text = "ABMAfiliados";
             this.btnABMAfiliado.UseVisualStyleBackColor = true;
@@ -53,9 +53,9 @@
             // 
             // btnABMRol
             // 
-            this.btnABMRol.Location = new System.Drawing.Point(126, 12);
+            this.btnABMRol.Location = new System.Drawing.Point(12, 12);
             this.btnABMRol.Name = "btnABMRol";
-            this.btnABMRol.Size = new System.Drawing.Size(75, 23);
+            this.btnABMRol.Size = new System.Drawing.Size(183, 40);
             this.btnABMRol.TabIndex = 1;
             this.btnABMRol.Text = "ABMRol";
             this.btnABMRol.UseVisualStyleBackColor = true;
@@ -63,9 +63,9 @@
             // 
             // btnABMProfesional
             // 
-            this.btnABMProfesional.Location = new System.Drawing.Point(240, 12);
+            this.btnABMProfesional.Location = new System.Drawing.Point(12, 104);
             this.btnABMProfesional.Name = "btnABMProfesional";
-            this.btnABMProfesional.Size = new System.Drawing.Size(93, 23);
+            this.btnABMProfesional.Size = new System.Drawing.Size(183, 40);
             this.btnABMProfesional.TabIndex = 2;
             this.btnABMProfesional.Text = "ABMProfesional";
             this.btnABMProfesional.UseVisualStyleBackColor = true;
@@ -73,9 +73,9 @@
             // 
             // btnCompraDeBono
             // 
-            this.btnCompraDeBono.Location = new System.Drawing.Point(12, 46);
+            this.btnCompraDeBono.Location = new System.Drawing.Point(12, 196);
             this.btnCompraDeBono.Name = "btnCompraDeBono";
-            this.btnCompraDeBono.Size = new System.Drawing.Size(121, 23);
+            this.btnCompraDeBono.Size = new System.Drawing.Size(183, 40);
             this.btnCompraDeBono.TabIndex = 3;
             this.btnCompraDeBono.Text = "Compra de Bono";
             this.btnCompraDeBono.UseVisualStyleBackColor = true;
@@ -83,9 +83,9 @@
             // 
             // btnRegistrarLlegada
             // 
-            this.btnRegistrarLlegada.Location = new System.Drawing.Point(12, 85);
+            this.btnRegistrarLlegada.Location = new System.Drawing.Point(12, 288);
             this.btnRegistrarLlegada.Name = "btnRegistrarLlegada";
-            this.btnRegistrarLlegada.Size = new System.Drawing.Size(99, 23);
+            this.btnRegistrarLlegada.Size = new System.Drawing.Size(183, 40);
             this.btnRegistrarLlegada.TabIndex = 4;
             this.btnRegistrarLlegada.Text = "Registrar Llegada";
             this.btnRegistrarLlegada.UseVisualStyleBackColor = true;
@@ -93,9 +93,9 @@
             // 
             // btnCancelarAtencion
             // 
-            this.btnCancelarAtencion.Location = new System.Drawing.Point(12, 124);
+            this.btnCancelarAtencion.Location = new System.Drawing.Point(12, 380);
             this.btnCancelarAtencion.Name = "btnCancelarAtencion";
-            this.btnCancelarAtencion.Size = new System.Drawing.Size(121, 23);
+            this.btnCancelarAtencion.Size = new System.Drawing.Size(183, 40);
             this.btnCancelarAtencion.TabIndex = 5;
             this.btnCancelarAtencion.Text = "Cancelar Atencion";
             this.btnCancelarAtencion.UseVisualStyleBackColor = true;
@@ -103,9 +103,9 @@
             // 
             // btnRegistrarAgenda
             // 
-            this.btnRegistrarAgenda.Location = new System.Drawing.Point(12, 163);
+            this.btnRegistrarAgenda.Location = new System.Drawing.Point(12, 150);
             this.btnRegistrarAgenda.Name = "btnRegistrarAgenda";
-            this.btnRegistrarAgenda.Size = new System.Drawing.Size(132, 23);
+            this.btnRegistrarAgenda.Size = new System.Drawing.Size(183, 40);
             this.btnRegistrarAgenda.TabIndex = 6;
             this.btnRegistrarAgenda.Text = "Registrar Agenda";
             this.btnRegistrarAgenda.UseVisualStyleBackColor = true;
@@ -113,9 +113,9 @@
             // 
             // btnListado
             // 
-            this.btnListado.Location = new System.Drawing.Point(170, 46);
+            this.btnListado.Location = new System.Drawing.Point(12, 426);
             this.btnListado.Name = "btnListado";
-            this.btnListado.Size = new System.Drawing.Size(120, 23);
+            this.btnListado.Size = new System.Drawing.Size(183, 40);
             this.btnListado.TabIndex = 7;
             this.btnListado.Text = "Listado Estadistico";
             this.btnListado.UseVisualStyleBackColor = true;
@@ -123,9 +123,9 @@
             // 
             // btnRegistroResultadoAtencion
             // 
-            this.btnRegistroResultadoAtencion.Location = new System.Drawing.Point(12, 204);
+            this.btnRegistroResultadoAtencion.Location = new System.Drawing.Point(12, 334);
             this.btnRegistroResultadoAtencion.Name = "btnRegistroResultadoAtencion";
-            this.btnRegistroResultadoAtencion.Size = new System.Drawing.Size(169, 23);
+            this.btnRegistroResultadoAtencion.Size = new System.Drawing.Size(183, 40);
             this.btnRegistroResultadoAtencion.TabIndex = 8;
             this.btnRegistroResultadoAtencion.Text = "Registro Resultado Atencion";
             this.btnRegistroResultadoAtencion.UseVisualStyleBackColor = true;
@@ -133,9 +133,9 @@
             // 
             // btnTurno
             // 
-            this.btnTurno.Location = new System.Drawing.Point(198, 123);
+            this.btnTurno.Location = new System.Drawing.Point(12, 242);
             this.btnTurno.Name = "btnTurno";
-            this.btnTurno.Size = new System.Drawing.Size(92, 23);
+            this.btnTurno.Size = new System.Drawing.Size(183, 40);
             this.btnTurno.TabIndex = 9;
             this.btnTurno.Text = "Pedido Turno";
             this.btnTurno.UseVisualStyleBackColor = true;
@@ -145,7 +145,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 289);
+            this.ClientSize = new System.Drawing.Size(208, 478);
             this.Controls.Add(this.btnTurno);
             this.Controls.Add(this.btnRegistroResultadoAtencion);
             this.Controls.Add(this.btnListado);
